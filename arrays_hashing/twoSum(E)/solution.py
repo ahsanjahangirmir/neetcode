447a5be90d736172884ded6657871c0e766ea86b
Ahsan J Mir <65661108+ahsanjahangirmir@users.noreply.github.com>
2024-09-10 23:24:11 +0500

    neetcode problems

---
+++ arrays_hashing/twoSum(E)/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        for i in range(len(nums)):
+            if target - nums[i] in nums[i+1:]: 
+                remaining = nums[i+1:]
+                return [i, i + 1 + remaining.index(target-nums[i])]
+        
+        return []

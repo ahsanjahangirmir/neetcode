8f87ab5e9577a2c5a6ed18884d740001b2ab51b5
Ahsan J Mir <65661108+ahsanjahangirmir@users.noreply.github.com>
2024-09-11 20:58:53 +0500

    neetcode problems

--- arrays_hashing/isAnagram(E)/solution.py
+++ arrays_hashing/isAnagram(E)/solution.py
@@ -1,11 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
-        if len(s) != len(t):
-            return False
-
-        s_letters = [s[i] for i in range(len(s))]
-        t_letters = [t[i] for i in range(len(t))]
-
-        return sorted(t_letters) == sorted(s_letters)
-        
+        return sorted(t) == sorted(s)

72ed7bc4c357e63208146f31b8a2a80e9b816558
Ahsan J Mir <65661108+ahsanjahangirmir@users.noreply.github.com>
2024-09-13 20:37:59 +0500

    neetcode problems

--- arrays_hashing/groupAnagrams(M)/solution.py
+++ arrays_hashing/groupAnagrams(M)/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
-        if len(strs) == 0 or len(strs) == 1:
+        if len(strs) <= 1:
             return [strs]
 
         hashmap = {}

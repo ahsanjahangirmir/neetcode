b80542e1a8245d44e4d510ea7cda93a44779375c
Ahsan J Mir <65661108+ahsanjahangirmir@users.noreply.github.com>
2024-09-21 20:29:27 +0500

    neetcode problems

--- twopointers/threeSum(M)/solution.py
+++ twopointers/threeSum(M)/solution.py
@@ -17,7 +17,9 @@ class Solution:
                     res.add((nums[i], nums[l], nums[r]))  
                     l += 1
 
-                if threeSum < 0: l += 1 
-                else: r -= 1
+                if threeSum < 0: 
+                    l += 1 
+                else: 
+                    r -= 1
         
         return [list(tup) for tup in res]

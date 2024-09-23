87d46fd524dfd9e6b99c334747302b9e05e29193
Ahsan J Mir <65661108+ahsanjahangirmir@users.noreply.github.com>
2024-09-23 19:29:36 +0500

    neetcode problems

--- cs4602/defuse-bomb-challenge/solution.py
+++ cs4602/defuse-bomb-challenge/solution.py
@@ -1,14 +1,9 @@
-#!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
 
-#
-# Complete the 'decrypt' function below.
-#
 # The function is expected to return an INTEGER.
 # The function accepts following parameters:
 #  1. INTEGER_ARRAY code_array
